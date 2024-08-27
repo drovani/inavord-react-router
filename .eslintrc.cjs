e04c26d56a49619503bf5247c47dd900f9da52d8
David Rovani <david@rovani.net>
2024-08-27 06:37:24 -0500

    Pop in the tslint + prettier plugin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -69,6 +69,7 @@ module.exports = {
         "plugin:@typescript-eslint/recommended",
         "plugin:import/recommended",
         "plugin:import/typescript",
+        "prettier"
       ],
     },
 
