fe37a22e76404d5f5fd58d43e3991bf9eb555d09
David Rovani <david@rovani.net>
2024-09-23 15:50:13 -0500

    Remove the equipment side menu and simplify the action bar.

--- app/routes/equipment.tsx
+++ app/routes/equipment.tsx
@@ -1,15 +1,9 @@
 import {
-    Dialog,
-    DialogBackdrop,
-    DialogPanel,
-    DialogTitle,
-    TransitionChild,
-} from "@headlessui/react";
-import { ChevronDoubleRightIcon, XMarkIcon } from "@heroicons/react/24/outline";
+    ArrowDownOnSquareIcon,
+    PlusIcon
+} from "@heroicons/react/24/outline";
 import { Button, Link } from "@nextui-org/react";
-import { Outlet, useLoaderData } from "@remix-run/react";
-import { useState } from "react";
-import ButtonBar from "~/components/ButtonBar";
+import { Outlet } from "@remix-run/react";
 import { getAllEquipment } from "../data";
 
 export const loader = async () => {
@@ -18,105 +12,36 @@ export const loader = async () => {
 };
 
 export default function Equipment() {
-    const { equipments } = useLoaderData<typeof loader>();
-    const [open, setOpen] = useState(false);
-
     return (
-        <div className="flex flex-row min-h-screen max-w-4xl">
-            <button
-                type="button"
-                onClick={() => setOpen(true)}
-                className="group flex flex-col items-start pt-3 bg-gray-700 text-white"
-            >
-                <ChevronDoubleRightIcon className="w-8 group-data-[open]:rotate-180" />
-                <div className="rotate-90 w-8 pl-1 text-3xl font-bold">
-                    Equipment
-                </div>
-            </button>
-            <Dialog open={open} onClose={setOpen} className="relative z-10">
-                <DialogBackdrop
-                    transition
-                    className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity duration-100 ease-in-out data-[closed]:opacity-0"
-                />
-                <div className="fixed inset-0 overflow-hidden">
-                    <div className="fixed inset-0 overflow-hidden">
-                        <div className="pointer-events-none fixed inset-y-0 left-0 flex max-w-full pr-10">
-                            <DialogPanel
-                                transition
-                                className="pointer-events-auto relative w-screen max-w-md transform transition duration-100 ease-in-out data-[closed]:-translate-x sm:duration-100 "
-                            >
-                                <TransitionChild>
-                                    <div className="absolute right-0 top-0 mr-8 flex pl-2 pt-4 duration-100 ease-in-out data-[closed]:opacity-0 sm:pl-4">
-                                        <button
-                                            type="button"
-                                            onClick={() => setOpen(false)}
-                                            className="relative rounded-md text-gray-300 hover:text-white focus:outline-none focus:ring-2 focus:ring-white"
-                                        >
-                                            <span className="absolute -inset-2.5" />
-                                            <span className="sr-only">
-                                                Close panel
-                                            </span>
-                                            <XMarkIcon
-                                                aria-hidden="true"
-                                                className="h-6 w-6"
-                                            />
-                                        </button>
-                                    </div>
-                                </TransitionChild>
-                                <div className="flex h-full flex-col bg-gray-700 text-white py-6 shadow-xl">
-                                    <div className="px-4 sm:px-6">
-                                        <DialogTitle
-                                            as={Link}
-                                            href="/equipment/"
-                                            className="text-3xl font-bold leading-6"
-                                            onClick={() => setOpen(false)}
-                                        >
-                                            Equipment
-                                        </DialogTitle>
-                                    </div>
-                                    <div className="relative mt-6 flex-1 px-4 sm:px-6">
-                                        {equipments.length ? (
-                                            <div className="flex flex-col items-start">
-                                                {equipments.map((equipment) => (
-                                                    <Link
-                                                        key={equipment.slug}
-                                                        href={`/equipment/${equipment.slug}`}
-                                                        onClick={() =>
-                                                            setOpen(false)
-                                                        }
-                                                    >
-                                                        {equipment.name}
-                                                    </Link>
-                                                ))}
-                                            </div>
-                                        ) : (
-                                            <p>No equipment found</p>
-                                        )}
-                                    </div>
-                                </div>
-                                <ButtonBar>
-                                    <Button
-                                        as={Link}
-                                        href="/equipment/new"
-                                        color="primary"
-                                        onPress={() => setOpen(false)}
-                                    >
-                                        New
-                                    </Button>
-                                    <Button
-                                        as={Link}
-                                        href="/equipment.json"
-                                        download={true}
-                                        color="secondary"
-                                    >
-                                        Export
-                                    </Button>
-                                </ButtonBar>
-                            </DialogPanel>
-                        </div>
-                    </div>
-                </div>
-            </Dialog>
+        <div className="grid grid-cols-[max-content_1fr]">
+            <div className="flex flex-col space-y-3 text-3xl font-bold bg-foreground pt-4 pl-1 text-background items-start">
+                <p className="[writing-mode:vertical-lr] mb-10">Equipment</p>
+                <Button
+                    as={Link}
+                    color="primary"
+                    href="/equipment/new"
+                    isIconOnly
+                    size="sm"
+                    radius="full"
+                    variant="ghost"
+                    title="Create new equipment"
+                >
+                    <PlusIcon />
+                </Button>
+                <Button
+                    as={Link}
+                    color="primary"
+                    href="/equipment.json"
+                    isIconOnly
+                    size="sm"
+                    radius="full"
+                    variant="light"
+                    title="Download JSON export"
+                    download
+                >
+                    <ArrowDownOnSquareIcon />
+                </Button>
+            </div>
             <div className="py-4 sm:py-6 lg:py-8 pl-4">
                 <Outlet />
             </div>
