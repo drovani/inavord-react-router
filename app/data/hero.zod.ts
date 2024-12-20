757d64a1a68c4602d40f2dfb204753de5335f2cb
David Rovani <david@rovani.net>
2024-12-20 12:51:17 -0600

    Create first pass at hero data.

---
+++ app/data/hero.zod.ts
@@ -0,0 +1,44 @@
+import { z } from "zod";
+
+export const HeroSchema = z.object({
+  name: z.string(),
+  class: z.enum(["tank", "warrior", "mage", "support", "control", "marksman", "healer"]),
+  faction: z.enum(["nature", "chaos", "honor", "eternity", "mystery", "progress"]),
+  main_stat: z.enum(["strength", "agility", "intelligence"]),
+  attack_type: z.array(z.enum(["physical", "magic", "pure"])),
+  artifact_team_buff: z.enum([
+    "armor",
+    "dodge",
+    "physical attack",
+    "magic attack",
+    "magic defense",
+    "armor penetration",
+    "crit hit chance",
+    "magic penetration",
+  ]),
+  stone_source: z.array(
+    z.enum([
+      "Arena Shop",
+      "Campaign",
+      "Heroic Chest",
+      "Outland Shop",
+      "Special Events",
+      "Dungeon Shop",
+      "Guild War Shop",
+      "Grand Arena Shop",
+      "Tower Shop",
+      "Highwayman Shop",
+      "Hydra Shop",
+      "Soul Shop",
+    ])
+  ),
+  order_rank: z.number().int().positive(),
+});
+
+export type HeroRecord = z.infer<typeof HeroSchema>;
+export type HeroClass = HeroRecord["class"];
+export type HeroFaction = HeroRecord["faction"];
+export type HeroMainStat = HeroRecord["main_stat"];
+export type HeroAttackType = HeroRecord["attack_type"][number];
+export type HeroArtifactTeamBuff = HeroRecord["artifact_team_buff"];
+export type HeroStoneSource = HeroRecord["stone_source"][number];
