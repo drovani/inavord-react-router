98168ea25c4cfa8d15f0942296bff7dc5f782e34
David Rovani <david@rovani.net>
2024-12-28 14:23:22 -0600

    Refactor from file route convention to explicit route file.. blah.
    But also implemented auth at the layout/route level instead of something wonky inside the route modules.

--- app/routes.ts
+++ app/routes.ts
@@ -1,4 +1,32 @@
-import { type RouteConfig } from "@react-router/dev/routes";
-import { flatRoutes } from "@react-router/fs-routes";
+import { index, layout, prefix, route, type RouteConfig } from "@react-router/dev/routes";
 
-export default flatRoutes() satisfies RouteConfig;
+export default [
+  index("./routes/_index.tsx"),
+  route("heroes", "./routes/heroes.tsx", [
+    index("./routes/heroes._index.tsx"),
+    route(":slug/edit?", "./routes/heroes.$slug.tsx"),
+  ]),
+  route("titans", "./routes/titans.tsx", [index("./routes/titans._index.tsx")]),
+  route("equipment", "./routes/equipment.tsx", [
+    index("./routes/equipment._index.tsx"),
+    route(":slug", "./routes/equipment.$slug.tsx"),
+    route(":slug/edit?", "./routes/equipment.$slug_.edit.tsx"),
+    route("new", "./routes/equipment.new.tsx"),
+  ]),
+  route("missions", "./routes/missions.tsx", [
+    index("./routes/missions._index.tsx"),
+    route(":missionId", "./routes/missions.$missionId.tsx"),
+  ]),
+  route("missions.json", "./routes/missions[.json].tsx"),
+  route("equipment.json", "./routes/equipment[.json].tsx"),
+  route("heroes.json", "./routes/heroes[.json].tsx"),
+  ...prefix("admin", [
+    layout("./layouts/ProtectedAdminLayout.tsx", [
+      route("setup", "./routes/admin.setup.tsx"),
+      route("/*", "./routes/admin.tsx"),
+    ]),
+  ]),
+  layout("./layouts/ProtectedUserLayout.tsx", [
+    route("account", "./routes/account.tsx", [index("./routes/account._index.tsx")]),
+  ]),
+] satisfies RouteConfig;
