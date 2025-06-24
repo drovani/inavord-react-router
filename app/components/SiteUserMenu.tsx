e6c08c134b1dfb1d7574ce6d0183ebda86e917b9
drovani <david@rovani.net>
2025-06-24 10:59:56 -0500

    Check for empty avatar before trying to render.

--- app/components/SiteUserMenu.tsx
+++ app/components/SiteUserMenu.tsx
@@ -30,8 +30,8 @@ export function SiteUserMenu() {
                   className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
                 >
                   <Avatar className="h-8 w-8 rounded-lg">
-                    {user.avatar &&
-                      <AvatarImage src={`${user.avatar}`} alt={user.name} />
+                    {user.avatar && user.avatar !== "" &&
+                      <AvatarImage src={user.avatar} alt={user.name} />
                     }
                     <AvatarFallback className="rounded-lg">{user.fallback}</AvatarFallback>
                   </Avatar>
@@ -51,7 +51,9 @@ export function SiteUserMenu() {
                 <DropdownMenuLabel className="p-0 font-normal">
                   <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                     <Avatar className="h-8 w-8 rounded-lg">
-                      <AvatarImage src={`${user.avatar}`} alt={user.name} />
+                      {user.avatar && user.avatar !== "" &&
+                        <AvatarImage src={user.avatar} alt={user.name} />
+                      }
                       <AvatarFallback className="rounded-lg">{user.fallback}</AvatarFallback>
                     </Avatar>
                     <div className="grid flex-1 text-left text-sm leading-tight">
